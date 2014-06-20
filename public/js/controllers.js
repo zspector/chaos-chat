8a87f19c266baa56930768a3789696b1e45be782
Zachary Spector <zjspector@gmail.com>
2014-06-20 08:37:21 -0500

    User box working. Still collects function names

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,18 +1,100 @@
-'use strict';
+app.controller('AppCtrl', ['$scope', 'socket', function($scope, socket) {
 
-/* Controllers */
+  $scope.messages = [];
+  $scope.users = [];
 
-angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, socket) {
-    socket.on('send:name', function (data) {
-      $scope.name = data.name;
-    });
-  }).
-  controller('MyCtrl1', function ($scope, socket) {
-    socket.on('send:time', function (data) {
-      $scope.time = data.time;
-    });
-  }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
+  // Socket listeners
+  socket.on('init', function (data) {
+    console.log(data)
+    $scope.name = data.name;
+    console.log(data.user)
+    $scope.users = data.user;
   });
+
+  socket.on('send:message', function (message) {
+    $scope.messages.push(message);
+  });
+
+  socket.on('change:name', function (data) {
+    changeName(data.oldName, data.newName);
+  });
+
+  socket.on('user:join', function (data) {
+    $scope.messages.push({
+      user: 'chatroom',
+      text: 'User ' + data.name + ' has joined'
+    });
+    $scope.users.push(data.name);
+    console.log("scope.users: ", $scope.users)
+  });
+
+  // add a message to the conversation when a user disconnects or leaves the room
+  socket.on('user:left', function (data) {
+    $scope.messages.push({
+      user: 'chatroom',
+      text: 'User ' + data.name + ' has left.'
+    });
+    var i, user;
+    for (i = 0; i < $scope.users.length; i++) {
+      user = $scope.users[i];
+      if (user === data.name) {
+        $scope.user.splice(i, 1);
+        break;
+      }
+    }
+  });
+
+  // Private helpers
+
+  var changeName = function (oldName, newName) {
+    // rename user in list of users
+    console.log('step 2 controller.js')
+    var i;
+    for (i = 0; i < $scope.users.length; i++) {
+      if ($scope.users[i] == oldName) {
+        $scope.users[i] = newName;
+      }
+    }
+
+    $scope.messages.push({
+      user: 'chatroom',
+      text: 'User ' + oldName + ' is now known as ' + newName + '.'
+    });
+  }
+
+  // Methods published to the scope
+
+  $scope.changeName = function () {
+    socket.emit('change:name', {
+      name: $scope.newName
+    }, function (result) {
+      if (!result) {
+        alert('There was an error changing your name');
+      } else {
+        console.log("controller:75 changeName $scope.name:",$scope.name);
+        console.log("controller:76 changeName $scope.newName:",$scope.newName);
+        console.log("controller:77 changeName $scope.user:",$scope.users);
+        changeName($scope.name, $scope.newName);
+
+        $scope.name = $scope.newName;
+        $scope.newName = '';
+      }
+    });
+  };
+
+  $scope.sendMessage = function () {
+    socket.emit('send:message', {
+      message: $scope.message
+    });
+
+    // add the message to our model locally
+    $scope.messages.push({
+      id: Math.round(Math.random() * 10000000 / 100),
+      user: $scope.name,
+      text: $scope.message
+    });
+
+    // clear message box
+    $scope.message = '';
+  };
+}]);
